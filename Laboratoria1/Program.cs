297cd8a522a3f69b552042bb5cab9d6ecc024973
KWeron <w70820@student.wsiz.edu.pl>
2024-11-19 20:09:05 +0100

    Zadanie 2

--- Laboratoria1/Program.cs
+++ Laboratoria1/Program.cs
@@ -1,11 +1,12 @@
-﻿internal class Program
-{
+﻿using System.Net;
 
+internal class Program
+{
     // 1 zadanie
     static void Main(string[] args)
     {
         Console.WriteLine("Podaj liczbe:");
-        var n = Convert.ToInt32(Console.ReadLine());
+        var n = int.Parse(Console.ReadLine());
 
         if (n % 2 == 0)
         {
@@ -13,7 +14,24 @@
         }
         else
         {
-            Console.WriteLine("Podana liczba jest liczbą nieparzystą");
+            Console.WriteLine("Podana liczba jest liczbą nieparzystą.");
         };
+   
+    WriteEven();
+    }
+
+    //zadanie 2
+    static void WriteEven()
+    {
+        Console.WriteLine("Podaj liczbę do której chcesz skończyć odliczanie liczb parzystych: ");
+        var n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= n; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
     }
 }
