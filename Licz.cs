2c0894fcc6bc20ae1714e4f5aaef40988010e45d
Karolina Weron <w70820@student.wsiz.edu.pl>
2025-01-30 10:38:11 +0100

    licz

---
+++ Licz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Licz
+{
+	private int wartość;
+
+	public Licz(int wartość)
+	{
+		this.wartość = wartość; 
+	}
+
+	public void Dodaj(int liczba) 
+	{
+		wartość += liczba;
+	}
+
+	public void Odejmij(int liczba)
+	{
+		wartość -= liczba;
+	}
+
+	public void WypisanieStanu()
+	{
+		Console.WriteLine($"Aktualny wartość: (wartość)");
+	}
+}
