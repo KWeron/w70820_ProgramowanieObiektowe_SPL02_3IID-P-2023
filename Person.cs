9650ee678b0f81abda5bc042655c9f4c72ccac58
Karolina Weron <w70820@student.wsiz.edu.pl>
2025-01-30 10:30:55 +0100

    osoba

--- Person.cs
+++ Person.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 
-public class Class1
+public class Osoba
 {
-	public Class1()
-	{
-		string[] cars = { "volvo", "bmw", "fiat" };
-		Console.WriteLine(cars[2]);
-	}
+    private string imie;
+    private string nazwisko;
+    private int wiek;
+    private string pesel;
+
+    public Osoba(string imie, string nazwisko, int wiek, string pesel)
+    {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        Wiek = wiek;
+        this.pesel = pesel;
+    }
+
+    public string Imie
+    {
+        get { return imie; }
+        set { imie = value; }
+    }
+
+    public string Nazwisko
+    {
+        get { return nazwisko; }
+        set { nazwisko = value; }
+    }
+
+    public int Wiek
+    {
+        get { return wiek; }
+        set
+        {
+            if (value < 0)
+                wiek = 0;
+            else
+                wiek = value;
+        }
+    }
+
+    public string Pesel
+    {
+        get { return pesel; }
+    }
+
+    public string PrzedstawSie
+    {
+        return $"Nazywam się {Imie} {Nazwisko} i mam {Wiek} lat.";
+    }
 }
